2a2f91d3b21d610a814c5146e4457820732e1f2e
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-09-11 17:39:02 -0300

    feat docker testing

--- JornadaMilhas.Test.Integracao/ContextoFixture.cs
+++ JornadaMilhas.Test.Integracao/ContextoFixture.cs
@@ -5,26 +5,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Testcontainers.MsSql;
 
 namespace JornadaMilhas.Test.Integracao
 {
-    public class ContextoFixture
+    public class ContextoFixture: IAsyncLifetime
     {
-        public JornadaMilhasContext Context { get; }
-        public ContextoFixture() 
-        {
-            var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
-           .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;" +
-               "Initial Catalog=JornadaMilhas;" +
-               "Integrated Security=True;" +
-               "Connect Timeout=30;" +
-               "Encrypt=False;" +
-               "Trust Server Certificate=False;" +
-               "Application Intent=ReadWrite;" +
-               "Multi Subnet Failover=False"
-           ).Options;
+        public JornadaMilhasContext Context { get; private set; }
+        private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder()
+            .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
+            .Build();
 
-           Context = new JornadaMilhasContext(options);
+        public async Task InitializeAsync()
+        {
+            await _msSqlContainer.StartAsync();
+            var options = new DbContextOptionsBuilder<JornadaMilhasContext>()
+           .UseSqlServer(_msSqlContainer.GetConnectionString()).Options;
+
+            Context = new JornadaMilhasContext(options);
+            Context.Database.Migrate();
+        }
+
+        public async Task DisposeAsync()
+        {
+            await _msSqlContainer.StopAsync();
         }
     }
 }
+//"Data Source=(localdb)\\MSSQLLocalDB;" +
+//               "Initial Catalog=JornadaMilhas;" +
+//               "Integrated Security=True;" +
+//               "Connect Timeout=30;" +
+//               "Encrypt=False;" +
+//               "Trust Server Certificate=False;" +
+//               "Application Intent=ReadWrite;" +
+//               "Multi Subnet Failover=False"
